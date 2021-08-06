2f33665396145b64d9d9a3c169f02a09fb51211b
coffmanjrp <coffmanjrp@gmail.com>
2021-08-06 16:25:54 -0700

    Create Labal component and set submit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
+import Label from './components/Label';
 import './App.scss';
 
 function App() {
+  const [value, setValue] = useState({ email: '', password: '' });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(value);
+    setValue({ email: '', password: '' });
+  };
+
   return (
     <div className="App">
       <div className="container">
         <h1>Please Login</h1>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <div className="form-control">
-            <input type="text" required />
-            <label>Email</label>
+            <input
+              type="text"
+              value={value.email}
+              onChange={(e) => setValue({ ...value, email: e.target.value })}
+              required
+            />
+            <Label label="Email" />
           </div>
           <div className="form-control">
-            <input type="password" required />
-            <label>Password</label>
+            <input
+              type="password"
+              value={value.password}
+              onChange={(e) => setValue({ ...value, password: e.target.value })}
+              required
+            />
+            <Label label="Password" />
           </div>
           <button className="btn">Login</button>
           <p className="text">
---
+++ src/components/Label.jsx
@@ -0,0 +1,15 @@
+const Label = ({ label }) => {
+  const letters = label.split('');
+
+  return (
+    <label>
+      {letters.map((letter, index) => (
+        <span key={index} style={{ transitionDelay: `${index * 50}ms` }}>
+          {letter}
+        </span>
+      ))}
+    </label>
+  );
+};
+
+export default Label;
