118aafe9e6b46265b5b0d569ce93be59590546f9
coffmanjrp <coffmanjrp@gmail.com>
2021-08-06 15:30:03 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <h1>Please Login</h1>
+        <form>
+          <div className="form-control">
+            <input type="text" required />
+            <label>Email</label>
+          </div>
+          <div className="form-control">
+            <input type="password" required />
+            <label>Password</label>
+          </div>
+          <button className="btn">Login</button>
+          <p className="text">
+            Don't have an account? <a href="#!">Register</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
